76d0d314dbae4e1a4df304fdaf60b66957d4254f
advayk <advay.koranne@gmail.com>
2019-12-09 22:47:53 -0800

    Working with 73 epochs

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Read_in_File read = new Read_in_File();
-     //   read.read_file("AndDataSet",100, 1, 2);
-        read.read_file("XORDataSet2",100, 0.05, 2);
+        read.read_file("AndDataSet",100, 1, 2);
+     //   read.read_file("XORDataSet2",100, 0.05, 2);
     }
 }
