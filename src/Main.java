9e6dc880238767cb404fc683f4cd095e7f105b1b
advayk <advay.koranne@gmail.com>
2019-12-17 17:59:11 -0800

    97% on handwritten training set

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         Read_in_File read = new Read_in_File();
-        read.read_in_training_file("HandwrittenTrainingSet",100, 0.06, 140);
+        read.read_in_training_file("HandwrittenTrainingSet",95, 0.06, 140);
       // read.read_file("XORDataSet2",100, 0.05, 2);
-        read.read_in_testing_file("HandwrittenTestingSet", 100, 0.06, 140);
+        read.read_in_testing_file("HandwrittenTestingSet", 95, 0.06, 140);
     //    read.read_in_training_file("HandwrittenTrainingSet",100, 0.06, 140);
 
     }
